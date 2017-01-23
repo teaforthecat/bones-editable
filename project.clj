b26d532eddd73126d67a930595eac93fe97b5c95
Chris Thompson <teaforthecat@gmail.com>
2017-01-22 19:10:39 -0600

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bones/editable "0.1.1"
+(defproject bones/editable "0.1.2"
   :description "lifecycle events for forms using re-frame"
   :url "https://github.com/teaforthecat/bones-editable"
   :license {:name "Eclipse Public License"
